156ed6850ecc359b9b4379bccbe0041ba06955f6
Robert <robertmamala@gmail.com>
2023-03-05 22:22:53 +0100

    Pierwsze pliki projektu serwera http na esp32. Init wifi oraz pierwszy prosty serwer.

---
+++ main/src/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/timers.h"
+#include "freertos/event_groups.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "esp_netif.h"
+#include "esp_http_server.h"
+
+#include "wifi.h"
+
+
+void app_main(void)
+{
+     nvs_flash_init();   
+	 wifi_connection();  
+}
